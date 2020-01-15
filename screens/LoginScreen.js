10187991b6e9284cb1fbd2f7ef279894c3919f23
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2020-01-15 01:58:08 +0100

    Fetch login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
 export default ({navigation: {navigate}}) => {
+  const [ login, setLogin ] = useState('');
+  const [ password, setPassword ] = useState('');
 
+  const sendLogin = async (login, password) => {
+    const msg = await fetch('http://192.168.1.12:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({login, password})
+    });
+    if(!msg.ok) return;
+    navigate('Map');
+  };
   return (
     <View style={styles.view}>
       <Text>Geotracker</Text>
-      <TextInput style={styles.input} placeholder="Login" />
-      <TextInput style={styles.input} placeholder="Password" />
+      <TextInput
+        style={styles.input}
+        value={login}
+        onChangeText={ t => setLogin(t) }
+        placeholder="Login"
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={ t => setPassword(t) }
+        placeholder="Password"
+        secureTextEntry
+      />
       <Button 
         title="Login"
-        onPress={() => navigate('Map')}
+        onPress={() => sendLogin(login, password)}
       />
     </View>
   );
